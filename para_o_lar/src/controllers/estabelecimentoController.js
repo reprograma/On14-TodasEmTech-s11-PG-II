cc8f094634e546600b90cb3b9e36f9d594a99b9f
Leticia Desiderio <letidesiderio18@gmail.com>
2021-10-28 15:56:26 -0300

    atualizar parte da atividade (vou construir readme ainda)

--- para_o_lar/src/controllers/estabelecimentoController.js
+++ para_o_lar/src/controllers/estabelecimentoController.js
@@ -18,105 +18,125 @@ const write = (request, response) => {
   );
 };
 const getAll = (request, response) => {
-  const { pagamento, bairro, delivery } = request.query;
+  const { pagamento, bairro, delivery, likes } = request.query;
 
-  let filtrados = models;
+  let filtered = models;
 
   if (pagamento) {
-    filtrados = filtrados.filter((estab) => {
+    filtered = filtered.filter((estab) => {
       return estab.pagamento.includes(pagamento);
     });
   }
 
   if (bairro) {
-    filtrados = filtrados.filter((estab) => {
+    filtered = filtered.filter((estab) => {
       return estab.bairro.includes(bairro);
     });
   }
 
   if (delivery) {
-    filtrados = filtrados.filter((estab) => {
+    filtered = filtered.filter((estab) => {
       return estab.delivery == (delivery == "true" ? true : false);
     });
   }
 
+  if (likes) {
+    filtered = filtered.filter((estab) => {
+      return estab.likes == likes;
+    });
+  }
   response.status(200).send(filtrados);
 };
 
 const getById = (request, response) => {
   const idRequest = request.params.id;
-  let estabelecimentoFound = models.find((estab) => estab.id == idRequest);
+  let foundRestaurant = models.find((estab) => estab.id == idRequest);
 
-  if (estabelecimentoFound == undefined) {
+  if (foundRestaurant == undefined) {
     response.status(404).json([
       {
-        message: "Estabelecimento não encontrado.",
+        message: "Este restaurante não foi encontrado.",
       },
     ]);
   }
 
-  response.status(200).send(estabelecimentoFound);
+  response.status(200).send(foundRestaurant);
 };
 
 const like = (req, res) => {
   const { id } = req.params;
-  const found = models.find((estab) => estab.id == id);
+  const foundRestaurant = models.find((estab) => estab.id == id);
 
-  if (found == undefined) {
-    res.status(404).send([{ message: "Estabelecimento não encontrado" }]);
+  if (foundRestaurant == undefined) {
+    res.status(404).send([{ message: "Este restaurante não foi encontrado." }]);
   }
 
-  found.likes += 1;
+  foundRestaurant.likes += 1;
   write();
-  res.status(200).send(found);
+  res.status(200).json([
+    {
+      message: `Obrigada pela colaboração, você curtiu o restaurante ${foundRestaurant.nome}.`,
+    },
+  ]);
 };
 
 const deslike = (req, res) => {
   const { id } = req.params;
-  const found = models.find((estab) => estab.id == id);
+  const foundRestaurant = models.find((estab) => estab.id == id);
 
-  if (found == undefined) {
-    res.status(404).send([{ message: "Estabelecimento não encontrado" }]);
+  if (foundRestaurant == undefined) {
+    res.status(404).send([{ message: "Este restaurante não foi encontrado." }]);
   }
 
-  found.deslikes += 1;
+  foundRestaurant.deslikes += 1;
   write();
-  res.status(200).send(found);
+  res.status(200).send([
+    {
+      message: `Poxaa que pena! Você não gostou do restaurante ${foundRestaurant.nome}.`,
+    },
+  ]);
 };
 
 const remover = (req, res) => {
   const { id } = req.params;
-  const found = models.find((estab) => estab.id == id);
-  console.log(found);
-  if (found == undefined) {
-    res.status(404).send([{ message: "Estabelecimento não encontrado" }]);
+  const foundRestaurant = models.find((estab) => estab.id == id);
+  if (foundRestaurant == undefined) {
+    res.status(404).send([{ message: "Este restaurante não foi encontrado." }]);
   } else {
-    const index = models.indexOf(found);
+    const index = models.indexOf(foundRestaurant);
     models.splice(index, 1);
   }
 
   write();
-  res.status(200).send([{ message: "Estabelecimento deletado com sucesso" }]);
+  res.status(200).send([
+    {
+      message: `O restaurante ${foundRestaurant.nome} foi removido com sucesso.`,
+    },
+  ]);
 };
 
 const atualizar = (req, res) => {
   const { id } = req.params;
-  const found = models.find((estab) => estab.id == id);
+  const foundRestaurant = models.find((estab) => estab.id == id);
   let body = req.body;
-  if (found == undefined) {
-    res.status(404).send([{ message: "Estabelecimento não encontrado" }]);
+  if (foundRestaurant == undefined) {
+    res.status(404).send([{ message: "Este restaurante não foi encontrado." }]);
   }
 
   body.id = id;
-  Object.keys(found).forEach((keys) => {
+  Object.keys(foundRestaurant).forEach((keys) => {
     if (!body[keys]) {
-      found[keys] = found[keys];
+      foundRestaurant[keys] = foundRestaurant[keys];
     } else {
-      found[keys] = body[keys];
+      foundRestaurant[keys] = body[keys];
     }
   });
 
-  res.status(200).send([{ message: "Estabelecimento atualizado com sucesso" }]);
+  res.status(200).send([
+    {
+      message: `O restaurante ${foundRestaurant.nome} atualizado com sucesso.`,
+    },
+  ]);
 
   write();
 };
@@ -125,9 +145,10 @@ const create = (request, response) => {
   const bodyReq = request.body;
   let estab = {
     id: models.length + 1,
+    nome: bodyReq.nome,
+    cnpj: bodyReq.cnpj,
     likes: bodyReq.likes,
     deslikes: bodyReq.deslikes,
-    nome: bodyReq.nome,
     categoria: bodyReq.categoria,
     endereço: bodyReq.endereço,
     bairro: bodyReq.bairro,
@@ -135,13 +156,16 @@ const create = (request, response) => {
     telefone: bodyReq.telefone,
     pagamento: bodyReq.pagamento,
     delivery: bodyReq.delivery,
+    comentário_opcional: bodyReq.comentário,
   };
 
   if (
+    !bodyReq.nome ||
+    !bodyReq.cnpj ||
     !bodyReq.likes ||
     !bodyReq.deslikes ||
-    !bodyReq.nome ||
     !bodyReq.endereço ||
+    !bodyReq.categoria ||
     !bodyReq.bairro ||
     !bodyReq.cidade ||
     bodyReq.pagamento.length === 0 ||
@@ -149,28 +173,55 @@ const create = (request, response) => {
   ) {
     response.status(201).json([
       {
-        message: `É obrigatório infomar quantidades dos likes, o nome, o endereço, o bairro, cidade, pagamento e delivery.`,
+        message:
+          "É obrigatório informar os seguintes campos para cadastrar seu restaurante:",
       },
+      "* Nome;",
+      "* CNPJ;",
+      "* Likes;",
+      "* Deslikes;",
+      "* Endereço;",
+      "* Categoria;",
+      "* Bairro;",
+      "* Cidade;",
+      "* Pagamento;",
+      "* Delivery.",
     ]);
   } else if (
     bodyReq.cidade === "Duque de Caxias" &&
+    bodyReq.categoria === "Restaurante" &&
     bodyReq.telefone.length == 14 &&
-    bodyReq.nome.length <= 15
+    bodyReq.nome.length <= 15 &&
+    bodyReq.cnpj.length == 19
   ) {
     models.push(estab);
 
     response.status(201).json([
       {
-        message: "O estabelecimento foi criado com sucesso.",
+        message: `O restaurante ${estab.nome} cadastrado com sucesso.`,
         models,
       },
     ]);
   } else {
     response.status(201).json([
       {
-        message:
-          "Só podem ser cadastrados os estabelecimentos com nomes menores que 15 caracteres, o establecimento deverá ser de Duque de Caxias. Exemplo telefone: (xx) xxxx-xxxx",
+        main_message:
+          "É OBRIGATÓRIO SER RESTAURANTES PARA REALIZAR O CADASTRO!",
+        message: "OBS: LEIA OS TERMOS ABAIXO COM ATENÇÃO!",
       },
+      {
+        messaging_rules:
+          "É obrigatório realizar as regras abaixo para cadastrar seu restaurante:",
+      },
+      "*  Seu restaurante deverá estar localizado em Duque de Caxias;",
+      "*  O nome do seu restaurante deverá ser menor ou igual à 15 caracteres;",
+      "* O comentário deverá ser menor ou igual à 100 caracteres. ",
+      {
+        example_messages: "Exemplos de como inserir os seguintes campos:",
+      },
+      "* Comentário: Deixe seu feedback sobre o restaurante.",
+      "*  Telefone: (xx) xxxx-xxxx;",
+      "*  CNPJ: xx.xxx.xxx/xxxx-xx.",
     ]);
   }
 
